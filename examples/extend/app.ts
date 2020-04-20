4525958409d50443ce9797feac5d640445276cf4
HarryHao <koler778@gmail.com>
2020-04-20 12:00:28 +0800

    响应数据支持泛型

--- examples/extend/app.ts
+++ examples/extend/app.ts
@@ -1,44 +1,70 @@
 import axios from '../../src/index'
 
-axios({
-  url: '/extend/get',
-  method: 'get',
-  data: {
-    msg: '1'
+// axios({
+//   url: '/extend/get',
+//   method: 'get',
+//   data: {
+//     msg: '1'
+//   }
+// })
+
+// axios.request({
+//   url: '/extend/post',
+//   method: 'post',
+//   data: {
+//     msg: 'hello'
+//   }
+// })
+
+// axios.get('/extend/get')
+
+// axios.post('/extend/post', { msg: 'post' })
+
+// axios.options('/extend/options')
+
+// axios.delete('/extend/delete')
+
+// axios.put('/extend/put', { msg: 'put' })
+
+// axios.patch('/extend/patch', { msg: 'patch' })
+
+// axios({
+//   url: '/extend/post',
+//   method: 'post',
+//   data: {
+//     msg: 'hi'
+//   }
+// })
+
+// axios('/extend/post', {
+//   method: 'post',
+//   data: {
+//     msg: 'hello'
+//   }
+// })
+
+interface ResponseData<T = any> {
+  code: number
+  result: T
+  message: string
+}
+
+interface User {
+  name: string
+  age: number
+}
+
+function getUser<T>() {
+  return axios<ResponseData<T>>('/extend/user')
+    .then(res => res.data)
+    .catch(err => console.error(err))
+}
+
+async function test() {
+  const user = await getUser<User>()
+  if(user) {
+    console.log(user.result.name);
   }
-})
+}
 
-axios.request({
-  url: '/extend/post',
-  method: 'post',
-  data: {
-    msg: 'hello'
-  }
-})
-
-axios.get('/extend/get')
-
-axios.post('/extend/post', { msg: 'post' })
-
-axios.options('/extend/options')
-
-axios.delete('/extend/delete')
-
-axios.put('/extend/put', { msg: 'put' })
-
-axios.patch('/extend/patch', { msg: 'patch' })
-
-axios({
-  url: '/extend/post',
-  method: 'post',
-  data: {
-    msg: 'hi'
-  }
-})
-
-axios('/extend/post', {
-  method: 'post',
-  data: {
-    msg: 'hello'
-  }
-})
+test()
